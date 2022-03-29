981d5ca1a3f9ace8bcce2c7a358a50e0c499286f
Philipp Flenker <philipp.klaus.flenker@zalando.de>
2022-03-29 15:27:12 +0200

    execute-shortcuts

--- src/Konzept/withShortcuts.tsx
+++ src/Konzept/withShortcuts.tsx
@@ -1,4 +1,4 @@
-import { Editor as SlateEditor, Range, Text } from "slate";
+import { Editor as SlateEditor, Range, Text, Transforms, Point } from "slate";
 import { Mark, Editor } from "./types";
 const SHORTCUT_MARK_MAP: Readonly<{
   [key: string]: Mark;
@@ -24,7 +24,33 @@ function handleInlineShortcuts(editor: Editor) {
           if (index === undefined || !value.length) {
             return;
           }
-          console.log(mark, value, index, path);
+          const selectionOffset = editor.selection!.anchor.offset;
+          const lastMatchingCharOffset = index + value.length + 2;
+          if (selectionOffset - 1 !== lastMatchingCharOffset) {
+            return;
+          }
+          let beginning: Point = {
+            path: path,
+            offset: index,
+          };
+
+          Transforms.delete(editor, { at: beginning });
+
+          let end = {
+            path: path,
+            offset: index + value.length,
+          };
+          Transforms.delete(editor, { at: end });
+          let update: { [key: string]: boolean } = {};
+          update[mark] = true;
+          Transforms.setNodes(editor, update, {
+            at: {
+              anchor: beginning,
+              focus: end,
+            },
+            match: (n) => Text.isText(n),
+            split: true,
+          });
         }
       );
     }
