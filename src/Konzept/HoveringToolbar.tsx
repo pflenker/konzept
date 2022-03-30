3f83a0f47e9573f98dff17351405efa46c2c84cd
Philipp Flenker <philipp.klaus.flenker@zalando.de>
2022-03-30 13:52:57 +0200

    hovering-toolbar

--- src/Konzept/HoveringToolbar.tsx
+++ src/Konzept/HoveringToolbar.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import { Editor as SlateEditor, Range } from "slate";
 import { useFocused, useSlate } from "slate-react";
 
@@ -5,24 +6,48 @@ export default function HoveringToolbar() {
   const editor = useSlate();
   const inFocus = useFocused();
   let isVisible = false;
-
+  const ref = useRef<HTMLSpanElement | null>(null);
+  let coordinates = {
+    top: 0,
+    left: 0,
+  };
+  const domSelection = window.getSelection();
+  const el = ref.current;
   const { selection } = editor;
 
   if (
     inFocus &&
     selection &&
     Range.isExpanded(selection) &&
-    SlateEditor.string(editor, selection) !== ""
+    SlateEditor.string(editor, selection) !== "" &&
+    domSelection &&
+    el
   ) {
     isVisible = true;
-  }
-  if (!isVisible) {
-    return null;
+    const domRange = domSelection.getRangeAt(0);
+
+    const rect = domRange.getBoundingClientRect();
+    const top = rect.top - el.offsetHeight;
+    const left = rect.left - el.offsetWidth / 2 + rect.width / 2;
+    coordinates = {
+      top,
+      left,
+    };
   }
   return (
     <span
       onMouseDown={(e) => e.preventDefault()}
-      style={{ border: "1px dashed lightgray", padding: "7px" }}
+      ref={ref}
+      style={{
+        border: "1px dashed lightgray",
+        padding: "7px",
+        position: "fixed",
+        zIndex: 1,
+        backgroundColor: "white",
+        top: coordinates.top + "px",
+        left: coordinates.left + "px",
+        visibility: isVisible ? "visible" : "hidden",
+      }}
     >
       <button>
         <mark>H</mark>
