467c1993c521ee4edcc16941b50663bb749e557f
Philipp Flenker <philipp.klaus.flenker@zalando.de>
2022-04-02 15:00:53 +0200

    extend-paragraph-type

--- src/Konzept/types.tsx
+++ src/Konzept/types.tsx
@@ -9,10 +9,11 @@ type Text = {
   highlight?: boolean;
 };
 export type Mark = "bold" | "highlight" | "italic" | "strikethrough";
-
+export type Decoration = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 export type Editor = BaseEditor & ReactEditor;
 type Paragraph = {
   type: "paragraph";
+  decoration?: Decoration;
   children: Text[];
 };
 declare module "slate" {
